d9be904e9cc44f7d0a1d031dd37c51d32b83843f
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-21 19:20:49 -0300

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 @REM ===========================================================================
 
 set "windowsTerminalConfigSource=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-set "windowsTerminalConfigDestination=%USERPROFILE%\Source\repos\windots\wt\settings.json"
+set "windowsTerminalConfigDestination=%USERPROFILE%\source\repos\windots\wt\settings.json"
 
 if exist "%windowsTerminalConfigSource%" (
     del "%windowsTerminalConfigSource%"
@@ -12,10 +12,24 @@ mklink "%windowsTerminalConfigSource%" "%windowsTerminalConfigDestination%"
 
 @REM ===========================================================================
 set "gitConfigSource=%USERPROFILE%\.gitconfig"
-set "gitConfigDestination=%USERPROFILE%\Source\repos\windots\.gitconfig"
+set "gitConfigDestination=%USERPROFILE%\source\repos\windots\.gitconfig"
 
 if exist "%gitConfigSource%" (
     del "%gitConfigSource%"
 )
 
 mklink "%gitConfigSource%" "%gitConfigDestination%"
+
+@REM ===========================================================================
+set "zedConfigSource=%APPDATA%\zed"
+set "zedConfigDestination=%USERPROFILE%\source\windots\zed"
+
+if exist "%zedConfigSource%" (
+    rmdir /S /Q "%zedConfigSource%"
+)
+
+if not exist "%zedConfigDestination%" (
+    mkdir "%zedConfigDestination%"
+)
+
+mklink /D "%zedConfigSource%" "%zedConfigDestination%"
