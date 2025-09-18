26f733863e7a878834572d47474d8150ecf86606
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-09-17 21:47:15 -0300

    change path

--- deploy.cmd
+++ deploy.cmd
@@ -6,8 +6,8 @@ winget install Discord.Discord
 winget install Git.Git
 winget install Logitech.GHUB
 winget install Microsoft.PowerToys
-winget install Microsoft.VisualStudio.2022.Community
-winget install Microsoft.VisualStudioCode
+@REM winget install Microsoft.VisualStudio.2022.Community
+@REM winget install Microsoft.VisualStudioCode
 winget install Valve.Steam
 winget install Wacom.WacomTabletDriver
 winget install Zen-Team.Zen-Browser
@@ -15,7 +15,7 @@ winget install Zen-Team.Zen-Browser
 @REM ===========================================================================
 
 set "windowsTerminalConfigSource=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-set "windowsTerminalConfigDestination=%USERPROFILE%\source\repos\windots\wt\settings.json"
+set "windowsTerminalConfigDestination=%USERPROFILE%\.windots\wt\settings.json"
 
 if exist "%windowsTerminalConfigSource%" (
     del "%windowsTerminalConfigSource%"
@@ -25,7 +25,7 @@ mklink "%windowsTerminalConfigSource%" "%windowsTerminalConfigDestination%"
 
 @REM ===========================================================================
 set "gitConfigSource=%USERPROFILE%\.gitconfig"
-set "gitConfigDestination=%USERPROFILE%\source\repos\windots\.gitconfig"
+set "gitConfigDestination=%USERPROFILE%\.windots\.gitconfig"
 
 if exist "%gitConfigSource%" (
     del "%gitConfigSource%"
@@ -35,7 +35,7 @@ mklink "%gitConfigSource%" "%gitConfigDestination%"
 
 @REM ===========================================================================
 set "zedConfigSource=%APPDATA%\zed"
-set "zedConfigDestination=%USERPROFILE%\source\repos\windots\zed"
+set "zedConfigDestination=%USERPROFILE%\.windots\zed"
 
 if exist "%zedConfigSource%" (
     rmdir /S /Q "%zedConfigSource%"
@@ -49,7 +49,7 @@ mklink /D "%zedConfigSource%" "%zedConfigDestination%"
 
 @REM ===========================================================================
 set "vimConfigSource=%USERPROFILE%\.vimrc"
-set "vimDestination=%USERPROFILE%\source\repos\windots\vim\.vimrc"
+set "vimDestination=%USERPROFILE%\.windots\vim\.vimrc"
 
 if exist "%vimConfigSource%" (
     del "%vimConfigSource%"
