4ee98e859dfc2b340c49b11803513ffa324fc540
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-24 20:39:57 -0300

    wezterm: open new window on center

--- config/wezterm/config.lua
+++ config/wezterm/config.lua
@@ -98,4 +98,14 @@ M.launch_menu = {
   },
 }
 
+wezterm.on("gui-startup", function(cmd)
+  local screen = wezterm.gui.screens().main
+  local ratio = 0.5
+  local width, height = screen.width * ratio, screen.height * ratio
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {
+    position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
+  })
+  window:gui_window():set_inner_size(width, height)
+end)
+
 return M
