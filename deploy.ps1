21aceca568e0801fd22afe9221c1063d7ec2253d
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-02-08 20:19:17 -0300

    New script for deploy on clean Windows installation.

--- deploy.ps1
+++ deploy.ps1
@@ -1,62 +1,72 @@
-# ------------------------------------------------------------------------------
-# Install scoop
-# ------------------------------------------------------------------------------
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+# --------------------------------------------------------------------------
+# Create symbolic links because i'm used to use GNU Stow on *nix.
+# --------------------------------------------------------------------------
+function make-link ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+function make-farm () {
+    $userprofile = $env:userprofile
+    make-link "${userprofile}\.dotfiles\config\powershell\profile.ps1" "${userprofile}\Documents\Powershell\Microsoft.PowerShell_profile.ps1"
+    make-link "${userprofile}\.dotfiles\config\vim\.vimrc" "${userprofile}\.vsvimrc"
+    make-link "${userprofile}\.dotfiles\config\wt\settings.json" "${userprofile}\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+    make-link "${userprofile}\.dotfiles\config\git\config" "${userprofile}\.gitconfig"
+}
+
 
 # ------------------------------------------------------------------------------
-# Update scoop
+# Install, update and add scoop packages.
 # ------------------------------------------------------------------------------
-scoop install git
-scoop update
+function scoop-install () {
+    # Install scoop
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+    # Update scoop (its need git to update itself)
+    scoop install git
+    scoop update
+
+    # Add buckets
+    scoop bucket add extras
+    scoop bucket add versions
+
+    # Install packages
+    scoop install 7zip
+    scoop install cmake
+    scoop install discord
+    scoop install fastfetch
+    scoop install gimp
+    scoop install llvm
+    scoop install powertoys
+    scoop install pwsh
+    scoop install steam
+    scoop install zen-browser
+    scoop install zoxide
+}
+
 
 # ------------------------------------------------------------------------------
-# Add buckets
+# Update and add winget packages.
+# NOTE: I only use winget for packages that has a specific problem with scoop.
 # ------------------------------------------------------------------------------
-scoop bucket add extras
-scoop bucket add games
-scoop bucket add nerd-fonts
-scoop bucket add versions
+function winget-install () {
+    # Update winget
+    winget update
+    winget upgrade --all
 
-# ------------------------------------------------------------------------------
-# Install packages
-# ------------------------------------------------------------------------------
-scoop install 7zip
-scoop install bat
-scoop install cmake
-scoop install discord
-scoop install fastfetch
-scoop install fd
-scoop install ffmpeg
-scoop install file
-scoop install fzf
-scoop install ghostscript
-scoop install imagemagick
-scoop install innounp
-scoop install iosevka-nf
-scoop install jq
-scoop install less
-scoop install llvm
-scoop install lua-language-server
-scoop install neovim-nightly
-scoop install ninja
-scoop install nu
-scoop install pcsx2
-scoop install poppler
-scoop install powertoys
-scoop install ppsspp
-scoop install pwsh
-scoop install resvg
-scoop install ripgrep
-scoop install steam
-scoop install vcredist
-scoop install vcredist2005
-scoop install vcredist2008
-scoop install vcredist2010
-scoop install vcredist2012
-scoop install vcredist2013
-scoop install vcredist2022
-scoop install wezterm-nightly
-scoop install windhawk
-scoop install yazi
-scoop install zoxide
+    # Install packages
+    winget install AgileBits.1Password
+    winget install Logitech.GHUB
+    winget install Microsoft.VisualStudio.Community
+    winget install Microsoft.VisualStudioCode
+    winget install RamenSoftware.Windhawk
+    winget install Wacom.WacomTabletDriver
+}
+
+
+# --------------------------------------------------------------------------
+# Run all
+# --------------------------------------------------------------------------
+make-farm
+scoop-install
+winget-install
