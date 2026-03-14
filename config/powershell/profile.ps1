21ba6e294eee361cce82f1ce04ac62d54233c98b
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-03-13 21:54:08 -0300

    powershell: disable zoxide

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -22,4 +22,4 @@ function prompt {
 # }
 # Invoke-Expression (&starship init powershell)
 
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+# Invoke-Expression (& { (zoxide init powershell | Out-String) })
